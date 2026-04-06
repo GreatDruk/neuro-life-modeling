32fc4be31fd9ccef23670e74319c7d6764505699
Seva Peretiatko <peretyatkosewa06@mail.ru>
2026-04-06 23:42:43 +0300

    feat(man): create flight and draw functions

--- src/js/man.js
+++ src/js/man.js
@@ -1,26 +1,43 @@
 class Man {
     constructor(
-        width=15, height=25, round=5, lineWidth=2,
-        fillColor="#8c00a9", strokeColor="#000"
+        x=10, y=10, vx=0, vy=0, width=15, height=25, round=5,
+        lineWidth=1, fillColor="#8c00a9", strokeColor="#000"
     ) {
-        this.width = width
-        this.height = height
-        this.round = round
-        this.lineWidth = lineWidth
-        this.fillColor = fillColor
-        this.strokeColor = strokeColor
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+
+        this.width = width;
+        this.height = height;
+        this.round = round;
+        this.lineWidth = lineWidth;
+        this.fillColor = fillColor;
+        this.strokeColor = strokeColor;
     }
 
-    comeIntoTheWorld(x, y, ctx) {
-        var man = new Path2D();
+    comeIntoTheWorld() {
+        population.push(this);
+    }
 
+    be() {
         ctx.fillStyle = this.fillColor;
         ctx.strokeStyle = this.strokeColor;
         ctx.lineWidth = this.lineWidth;
 
-        man.roundRect(x, y, this.width, this.height, this.round)
-        ctx.stroke(man);
-        ctx.fill(man);
+        ctx.beginPath();
+        ctx.roundRect(this.x, this.y, this.width, this.height, this.round)
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    flight() {
+        if ((0 < this.x + this.vx) && (this.width + this.x + this.vx < field.width)) {
+            this.x += this.vx;
+        }
+        if ((0 < this.y + this.vy) && (this.height + this.y + this.vy < field.height)) {
+            this.y += this.vy;
+        }
     }
 
     bringIntoTheWorld() {
